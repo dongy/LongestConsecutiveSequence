c49cd5fa0ca498f589fc6c1d9ed1ab8259a850fc
dongy <doyan@nets.eu>
2020-01-02 21:51:42 +0100

    use hashmap to calculate longest consecutive sequence

--- src/Run3.java
+++ src/Run3.java
@@ -36,12 +36,20 @@ public class Run3 {
 		map.put(9, 9);
 		map.put(3, 3);
 		map.put(19, 19);
+		map.put(18, 18);
+		map.put(17, 17);
+		map.put(21, 21);
+		map.put(22, 22);
+		map.put(5, 5);
+		map.put(-1, -1);
+		map.put(-3, -3);
+		map.put(-2, -2);
 		
 		List<Integer> curr = new ArrayList<>();
 		List<Integer> longest = new ArrayList<>();
 		
 		// [10, 4, 20, 1, 2, 8, 9, 3, 19]
-		List<Integer> key = new ArrayList<>();
+		/*List<Integer> key = new ArrayList<>();
 		key.add(10);
 		key.add(4);
 		key.add(20);
@@ -51,65 +59,65 @@ public class Run3 {
 		key.add(9);
 		key.add(3);
 		key.add(19);
+		key.add(5);
+		key.add(18);
+		key.add(17);
+		key.add(21);
+		key.add(22);*/
 		
 		// for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
 			// System.out.println("value=" + entry.getValue());
 			//System.out.println("map=" + map);
 		// }
 		System.out.println("map=" + map);
-		Map<Integer, Integer> visited = new ConcurrentHashMap<Integer, Integer>();
 		
 		// iterate map
-		Iterator<Map.Entry<Integer, Integer>> iter = map.entrySet().iterator();
-		// for(int i=map.size()-1; i>=0; i--) {
-		while ( iter.hasNext() ) {
+		//Iterator<Map.Entry<Integer, Integer>> iter = map.entrySet().iterator();
+		int mapSize = map.size();
+		for(int i=0; i<=map.size(); i++) {
+		//while ( iter.hasNext() ) {
 			System.out.println("map.size()=" + map.size());
-			//System.out.println("i=" + i);
-			//System.out.println("key.get(i)=" + key.get(i));
-			//System.out.println("key.get(i)=" + map.get(i));
-			
-			Map.Entry<Integer, Integer> entry = iter.next();
+			System.out.println("i=" + i);
+			//Map.Entry<Integer, Integer> entry = iter.next();
+			// always the first value
+			Map.Entry<Integer,Integer> entry = map.entrySet().iterator().next();
 			int first = entry.getValue();
 			System.out.println("first=" + first);
-			if ( ! visited.containsKey(first) ) {
-				curr.add(first);
-				visited.put(first, first);
-				map.remove(first);
-				//iter.remove();
-				System.out.println("map1=" + map);
-				int small = first-1;
-				int big = first+1;
+			curr.add(first);
+			System.out.println("curr=" + curr);
+			map.remove(first);
+			//iter.remove();
+			System.out.println("map1=" + map);
+			int small = first-1;
+			int big = first+1;
 				
-				for(int j=0; j<map.size(); j++) {		
-					System.out.println("j=" + j);
-					System.out.println("small=" + small);
-					System.out.println("big=" + big);
-					// if map has a key == first-1 
-					if(map.containsKey(small)) {
-						curr.add(small);
-						//System.out.println("small=" + small);
-						map.remove(small);
-						System.out.println("map2=" + map);
-						small = small-1;
-					}else if(map.containsKey(big)) {
-						curr.add(big);
-						//System.out.println("big=" + big);
-						map.remove(big);
-						System.out.println("map3=" + map);
-						big = big+1;
-					}else {
-						System.out.println("break1");
-						break;
-					}
+			for(int j=0; j<mapSize; j++) {		
+				System.out.println("j=" + j);
+				// if map has a key == first-1 
+				if( map.containsKey(small) ) {
+					curr.add(small);
 					System.out.println("curr=" + curr);
+					System.out.println("small=" + small);
+					map.remove(small);
+					System.out.println("map2=" + map);
+					small = small-1;
 				}
-				if( curr.size() > longest.size()) {
-					longest = new ArrayList<>(curr);
-					System.out.println("longest=" + longest);
-					curr.clear();
+				// if map has a key == first+1
+				if( map.containsKey(big) ) {
+					curr.add(big);
+					System.out.println("curr=" + curr);
+					System.out.println("big=" + big);
+					map.remove(big);
+					System.out.println("map3=" + map);
+					big = big+1;
 				}
 			}
-		}
+			if( curr.size() > longest.size()) {
+				longest = new ArrayList<>(curr);
+				System.out.println("longest=" + longest);
+			}
+			curr.clear();
+		}	
 		
 		System.out.println("longest1=" + longest);
 		// longest.forEach(System.out::println);		
